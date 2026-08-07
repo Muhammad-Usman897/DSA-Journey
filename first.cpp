58d221598511326afca141aeb7474984e7804051
Muhammad Usman <itxusman568@gmail.com>
2026-08-07 14:40:27 +0500

    Day 1: Start DSA journey

---
+++ first.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //write first program
+    cout<<"Muhammad Usman"<<endl;
+
+    //------ Data types and varibles
+    int a; //variable decleration
+    a=10; //variable define
+    cout<<a<<endl;
+
+    //char
+    char c='a';
+    cout<<c<<endl;
+
+    //float
+    float f=2.14;
+    cout<<f<<endl;
+
+    //double
+    double d=3.143572;
+    cout<<d<<endl;
+
+    //boolean
+    bool b=true;
+    cout<<b;
+};
