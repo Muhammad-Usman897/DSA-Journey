53162d6befa170ad4de447a9801508c6e761b60d
Muhammad Usman <itxusman568@gmail.com>
2026-08-15 23:16:32 +0500

    Day 9: While Loop, do-while, break, continue, switch

--- while.cpp
+++ while.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
 
+// ---------- While Loop ----------
 // print talbe using while loop
 int num,i=1;
 cout<<"Enter the num: ";
@@ -13,13 +14,66 @@ while (i<=10)
 }
 
 //print factor of 10 using while loop
-int i=1;
-while(i<=10){
-    if(10%i==0){
-        cout<<i<<endl;
+int ii=1;
+while(ii<=10){
+    if(10%ii==0){
+        cout<<ii<<endl;
     }
-    i++;
+    ii++;
 }
 
+
+    // ---------- do-While Loop ----------
+    //print sum of n natural number using do-while loop
+    int sum=0,x=1;
+    do
+    {
+        sum=sum+x;
+        x++;
+    } while (x<=10);
+    cout<<"The sum of: "<<sum<<endl;
+
+    // ---------- break, continue ----------
+    //break
+    for(int i=1;i<=10;i++){
+        if(i==4){
+            break;
+        }
+        cout<<i<<" ";
+    }
+    cout<<endl;
+
+    //continue
+    for(int i=1;i<=10;i++){
+        if(i%4==0)
+        continue;
+        cout<<i<<" ";
+    }
+    
+    // ---------- switch ----------
+    int z=1;
+    switch (z)
+    {
+    case 1:
+        cout<<"Usman";
+        break;
+
+    case 2:
+        cout<<"Furqan";
+        break;
+    
+    default:
+        cout<<"Jamal";
+        break;
+    }
+
+    // ---------- Scope of a varible ----------
+    int n=10;
+    int y=20;
+    cout<<n;
+    if(y==20){
+        int n=30;
+        cout<<n;
+    }
 
 }
