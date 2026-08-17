beeb69060082daafccf26a1c3fc6955a4924f1c1
Muhammad Usman <itxusman568@gmail.com>
2026-08-17 19:59:20 +0500

    Day 11: Solve LeetCode Power of 2

---
+++ Day-11/Leetcode/powerOfTwo.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the num: ";
+    cin>>n;
+    if(n<1)
+    return 0;
+
+    while(n>1){
+        if(n%2==1)
+            return 0;
+        n=n/=2;
+    }
+   cout<<1;
+}
