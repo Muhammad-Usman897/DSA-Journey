3d7c712669a13482305784229d0bd7c832a52f21
Muhammad Usman <itxusman568@gmail.com>
2026-08-30 23:22:26 +0500

    Day 23

---
+++ Day-23/peakIndexInMountainArray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={2,4,5,7,10,14,3,2,1};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int start=0, end=n-1, mid, ans;
+
+    while(start<=end){
+        mid=start+(end-start)/2;
+
+        if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]){
+        ans=mid;
+            break;
+        }
+        else if(arr[mid]>arr[mid-1])
+        start=mid+1;
+        else
+        end=mid-1;
+    }
+    cout<<ans;
+}
